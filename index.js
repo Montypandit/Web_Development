b6d999879787cf51f5708694de95d0e636c9ddcc
monty787 <monty.2426mca1965@kiet.edu>
2025-11-25 14:11:53 +0530

    update index file

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const public = path.join(__dirname);
 app.get('/', (req, res) => {
     res.send(` Welcome to Node js server `);
 });
+app.get("/user",(req,res)=>{
+    const user=req.query.user;
+const pwd=req.query.pwd;
+if(user==="admin" && pwd==="admin123"){
+    res.send("Login Successful");
+} else {
+    res.send("Invalid Credentials");
+}
+});
 app.get('/about', (req, res) => {
     res.send(` This is about page of website`);
 });
